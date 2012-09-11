e7d42b1f28e5631f5e4da214538ea096a785ee52
Leandro <leandroloriato@Leandro-Loriatos-MacBook-Pro.local>
2012-09-10 21:29:50 -0300

    Initial code for hlink utility

---
+++ hlink.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+)
+
+func main() {
+    flag.Parse()
+
+    // receives arguments from command line
+	args := flag.Args()
+
+    // check if there are exactly two arguments (source and destination of hard link)
+	if len(args) != 2 {
+        log.Println(args)
+		log.Fatal("Link script must receive exactly two arguments: source and destination")
+	}
+
+	src := args[0]
+	dst := args[1]
+
+    // make a direct call to the syscall of current OS
+	if err := os.Link(src, dst); err != nil {
+        // if there is an error, prints error message and exits
+		log.Fatal(err)
+	}
+
+	log.Println("Link successfully created requested link")
+}
